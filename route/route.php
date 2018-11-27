2ca645277d7659d0771462948c25519fba12785f
codekaar <kharelbarun@gmail.com>
2018-11-27 18:27:38 +0545

    include API version in route

--- route/route.php
+++ route/route.php
@@ -4,9 +4,9 @@ use FastRoute\RouteCollector;
 
 return function (RouteCollector $r) {
 
-    $r->post('/feed/{feed_slug}/{user_id}/', 'FeedController@addActivity');
+    $r->post('/v1.0/feed/{feed_slug}/{user_id}/', 'FeedController@addActivity');
 
-    $r->post('/feed/{feed_slug}/{user_id}/follows/', 'FeedController@follow');
+    $r->post('/v1.0/feed/{feed_slug}/{user_id}/follows/', 'FeedController@follow');
 
-    $r->get('/feed/{feed_slug}/{user_id}/', 'FeedController@get');
+    $r->get('/v1.0/feed/{feed_slug}/{user_id}/', 'FeedController@get');
 };
