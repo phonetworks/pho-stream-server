7dd03b8f480701ae36e6dfaaec87fa6a217acdbd
codekaar <kharelbarun@gmail.com>
2018-11-27 18:26:17 +0545

    remove unused code

--- route/route.php
+++ route/route.php
@@ -4,8 +4,6 @@ use FastRoute\RouteCollector;
 
 return function (RouteCollector $r) {
 
-    $r->get('/', 'HomeController@index');
-
     $r->post('/feed/{feed_slug}/{user_id}/', 'FeedController@addActivity');
 
     $r->post('/feed/{feed_slug}/{user_id}/follows/', 'FeedController@follow');
