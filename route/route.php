b52d5998ebfd44d2c5b88b1ea90321a307b67e2a
Groups <esokullu@gmail.com>
2018-12-06 03:29:33 -0800

    enrich route

--- route/route.php
+++ route/route.php
@@ -9,4 +9,6 @@ return function (RouteCollector $r) {
     $r->post('/v1.0/feed/{feed_slug}/{user_id}/follows/', 'FeedController@follow');
 
     $r->get('/v1.0/feed/{feed_slug}/{user_id}/', 'FeedController@get');
+
+    $r->get('/v1.0/enrich/feed/{feed_slug}/{user_id}/', 'FeedController@get');
 };
